46a08bdc6fd92895e994f93e4c8488a1ab88b0dc
John Paulo Panganiban <olopangs@gmail.com>
2021-11-19 19:10:08 +0800

    finally!!!!!!!

--- m.ts
+++ m.ts
@@ -37,8 +37,8 @@ const data = [{
 ];
 
 const newFilterState = {
-    // return tayo ng isang item lang
     "type": {
+        "series": true,
         "imba": true
     },
     "genre": {
@@ -51,45 +51,27 @@ const newFilterState = {
         "No": true
     }
 }
-
-// const filterState = { 'series': true, 'comedy': true, }
-// const filterArray = [...filterState]
-// const newData = data.filter((item) => {
-//     for (const [filterIndex, filterValue] of Object.entries(filterState)) {
-//       console.log(filterIndex, filterValue)
-//     }
-// })
-// console.log(newData);
-
-const newData = data.reduce((previousValue, item, currentIndex) => {
-    console.log(previousValue)
-    const obj = {}
+const newData = data.filter((item) => {
     for (const [objectIndex, objectValue] of Object.entries(newFilterState)) {
-        console.log(item)
-        console.log(objectIndex)
         for (const [filterIndex, filterValue] of Object.entries(objectValue)) {
             switch (true) {
                 case (filterIndex === item.Type): {
-                    console.log(item, 'with series**************************************************')
-                    Object.assign(obj, item)
-                    break;
+                    console.log(item, `with ${filterIndex}**************************************************`)
+                    return true;
                 }
                 case (filterIndex === item.Genre): {
-                    console.log(item, 'with comedyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-                    Object.assign(obj, item)
-                    break;
+                    console.log(item, `with ${filterIndex}XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`)
+                    return true;
                 }
-                case (objectIndex === 'watched' && filterIndex === 'Yes'):
-                case (objectIndex === 'saved' && filterIndex === 'Yes'): {
-                    console.log(item, 'with Yes-----------------------------------------------------')
-                    Object.assign(obj, item)
-                    break;
+                case (objectIndex === 'watched' && item.Watched === 'True' && filterIndex === 'Yes'): {
+                    console.log('watched is true')
                 }
+                // case (objectIndex === 'saved' && filterIndex === 'Yes'): {
+                //     console.log(item, `with ${filterIndex}-----------------------------------------------------`)
+                //    return true;
+                // }
             }
         }
-
     }
-    return [obj]
-}, [])
-
+})
 console.log('new data', newData)
